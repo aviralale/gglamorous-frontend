4bfa8e642a63e8df6539562acf5b5f5b0d678104
Aviral Ale <aviralale@gmail.com>
2025-08-13 18:16:04 +0545

    Update HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,6 @@
 import DisplayCard from "@/comps/DisplayCard";
 import CategoryCollage from "@/comps/HomePageCategory";
-import ProductOfTheDay from "@/comps/ProductOfTheDay";
+// import ProductOfTheDay from "@/comps/ProductOfTheDay";
 import ProductSection from "@/comps/ProductSection";
 
 const HomePage = () => {
@@ -9,7 +9,7 @@ const HomePage = () => {
       <ProductSection type="new" />
       <DisplayCard image="https://images.unsplash.com/photo-1558769132-cb1aea458c5e?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
       <ProductSection type="sale" />
-      <ProductOfTheDay />
+{/*       <ProductOfTheDay /> */}
       <ProductSection type="new" />
       <CategoryCollage />
     </div>
